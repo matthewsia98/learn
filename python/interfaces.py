8cafa2e39ebe2130fa301ffef64e45c9377b8c44
Matthew Sia <matthewsia98@gmail.com>
2023-05-31 20:22:30 -0400

    clean up

--- python/interfaces.py
+++ python/interfaces.py
@@ -1,25 +1,44 @@
 import abc
+from abc import ABC, abstractmethod
 
 
-class Base(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
+class Interface(ABC):
+    @abstractmethod
     def foo(self):
-        raise NotImplementedError
+        pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def bar(self):
-        raise NotImplementedError
+        pass
 
 
-class Concrete(Base):
+class ConcreteInterface(Interface):
     def foo(self):
         print("Base.foo")
 
-    # We forget to declare bar() again, and the metaclass complains.
-    # def bar(self):
-    #     print("Base.bar")
+    def bar(self):
+        print("Base.bar")
 
 
-c = Concrete()
-c.foo()
-c.bar()
+i = ConcreteInterface()
+i.foo()
+i.bar()
+
+
+class AbstractClass(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def foo(self):
+        pass
+
+    def bar(self):
+        print("AbstractClass.bar")
+
+
+class ConcreteAbstractClass(AbstractClass):
+    def foo(self):
+        print("ConcreteAbstractClass.foo")
+
+
+a = ConcreteAbstractClass()
+a.foo()
+a.bar()
