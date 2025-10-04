9b86f49688c233c4c572a1dfc4a8f364266fe1a5
Matthew Sia <matthewsia98@gmail.com>
2025-10-04 15:29:17 -0400

    more stuff

---
+++ csharp/HttpStream/Server/AuthHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace Server;
+
+class AuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    private readonly Runtime runtime;
+    public AuthHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock,
+        Runtime runtime
+    ) : base(options, logger, encoder, clock)
+    {
+        this.runtime = runtime;
+    }
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (runtime.User is null)
+        {
+            return Task.FromResult(AuthenticateResult.Fail("Not logged in"));
+        }
+        
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, runtime.User),
+            new(ClaimTypes.Role, "Admin"),
+        };
+        var identity = new ClaimsIdentity(claims, Scheme.Name);
+        var principal = new ClaimsPrincipal(identity);
+
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
--- csharp/HttpStream/Server/Program.cs
+++ csharp/HttpStream/Server/Program.cs
@@ -1,14 +1,57 @@
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+using Server;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthentication("Custom")
+    .AddScheme<AuthenticationSchemeOptions, AuthHandler>("Custom", options => { });
+builder.Services.AddAuthorization();
+
+builder.Services.AddSingleton<Runtime>();
+
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/login", Login);
+app.MapGet("/logout", Logout);
+app.MapGet("/user", GetUser);
 app.MapGet("/chars", GetChars);
 app.MapGet("/people", GetPeople);
+app.MapGet("/people-json", GetPeopleJson);
 
 app.Run();
 
-async IAsyncEnumerable<char> GetChars()
+IResult Login(string name, Runtime runtime)
 {
-    var s = "Hello, World!";
+    runtime.User = name;
+    return Results.Redirect("/user");
+}
+
+IResult Logout(Runtime runtime)
+{
+    runtime.User = null;
+    return Results.Redirect("/user");
+}
+
+string GetUser(IHttpContextAccessor httpContextAccessor)
+{
+    return httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value ?? "Not logged in";
+}
+
+[Authorize]
+async IAsyncEnumerable<char> GetChars(IHttpContextAccessor httpContextAccessor)
+{
+    var user = GetUser(httpContextAccessor);
+    var s = $"Hello, {user}!";
     foreach (var c in s)
     {
         await Task.Delay(1000);
@@ -16,6 +59,7 @@ async IAsyncEnumerable<char> GetChars()
     }
 }
 
+[Authorize]
 async IAsyncEnumerable<Person> GetPeople()
 {
     List<Person> people = [
@@ -31,6 +75,41 @@ async IAsyncEnumerable<Person> GetPeople()
         await Task.Delay(1000);
         yield return person;
     }
+}
+
+
+[Authorize]
+async Task GetPeopleJson(IHttpContextAccessor httpContextAccessor)
+{
+    List<Person> people = [
+        new("John", "Doe"),
+        new("Jane", "Doe"),
+        new("Jack", "Doe"),
+        new("Jill", "Doe"),
+        new("Jim", "Doe"),
+    ];
+
+    var response = httpContextAccessor.HttpContext.Response;
+    response.ContentType = "application/json";
+
+    await response.StartAsync();
+
+    await using var stream = response.Body;
+    await stream.WriteAsync(Encoding.UTF8.GetBytes("["));
+
+    for (var i = 0; i < people.Count; i ++)
+    {
+        await JsonSerializer.SerializeAsync(stream, people[i]);
+        if (i < people.Count - 1)
+        {
+            await stream.WriteAsync(Encoding.UTF8.GetBytes(","));
+        }
+        await stream.FlushAsync();
+        await Task.Delay(1000);
+    }
+
+    await stream.WriteAsync(Encoding.UTF8.GetBytes("]"));
+    await stream.FlushAsync();
 }
 
 
